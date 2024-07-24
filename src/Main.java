b5b3f8b3a2443bac4d5532384c279ecf6e468d84
Jean Carlos Guzman <jeancarlosg93@gmail.com>
2024-07-24 15:33:00 -0400

    created methods to find S and E

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+        char[][] myarray = {
+                {'|','|','|','|','|',},
+                {'|','S','|','|','|',}, // s location = myarray [1,1];
+                {'|',' ','|','|','|',},
+                {'|',' ',' ','E','|',}, // e location = myarray [3,3]
+                {'|','|','|','|','|',}
+        };
+
+        int[] sLocation;
+        int[] eLocation;
+        List<List<Integer>> connectingNodes;
+
+
+        connectingNodes =adjacencyList(myarray);
+        for (List<Integer> connectingNode : connectingNodes) {
+            System.out.println(connectingNode);
         }
+        sLocation = findS(myarray);
+        System.out.println(sLocation[0] + " " + sLocation[1]);
+        eLocation = findE(myarray);
+        System.out.println(eLocation[0] + " " + eLocation[1]);
+
+
     }
+
+    public static List<List<Integer>> adjacencyList(char[][] myarray) {
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+
+        for (int i = 0; i < myarray.length; i++) {
+                 adjacencyList.add(new ArrayList<>());
+        }
+        for (int i = 0; i < myarray[0].length; i++) {
+            for (int j = 0; j < myarray.length; j++) {
+                if (myarray[i][j] == '|') {
+                    adjacencyList.get(i).add(j);
+                }
+            }
+        }
+            return adjacencyList;
+    }
+
+    public static int[] findS(char[][] maze){
+        int[] s = {0,0};
+        for (int i = 0; i < maze.length; i++) {
+             for (int j = 0; j < maze[i].length; j++) {
+                 if (maze[i][j] == 'S') {
+                     s = new int[]{i, j};
+
+                 }
+             }
+         }
+        return s;
+    }
+
+    public static int[] findE(char[][] maze){
+        int[] e = {0,0};
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                if (maze[i][j] == 'E') {
+                    e = new int[]{i, j};
+                }
+            }
+        }
+        return e;
+    }
+
+
 }
