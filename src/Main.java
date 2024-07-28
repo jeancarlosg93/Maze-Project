2668177ee44a6c4a9f3f97fb7dc95303488fc4be
Jean Carlos Guzman <jeancarlosg93@gmail.com>
2024-07-28 17:29:25 -0400

    explore neighbors

--- src/Main.java
+++ src/Main.java
@@ -17,35 +17,22 @@ public class Main {
         int[] sLocation;
         int[] eLocation;
         List<List<Integer>> connectingNodes;
-
-
-        connectingNodes =adjacencyList(myarray);
-        for (List<Integer> connectingNode : connectingNodes) {
-            System.out.println(connectingNode);
-        }
         sLocation = findS(myarray);
-        System.out.println(sLocation[0] + " " + sLocation[1]);
-        eLocation = findE(myarray);
-        System.out.println(eLocation[0] + " " + eLocation[1]);
+        System.out.println(myarray[sLocation[0]][sLocation[1]+1]);
+        System.out.println(myarray[sLocation[0]][sLocation[1]-1]);
+        System.out.println(myarray[sLocation[0]+1][sLocation[1]]);
+        System.out.println(myarray[sLocation[0]-1][sLocation[1]+1]);
+
+
 
 
     }
 
-    public static List<List<Integer>> adjacencyList(char[][] myarray) {
-        List<List<Integer>> adjacencyList = new ArrayList<>();
+//    public static List<List<Integer>> solve(int[]sLocation, char[][] maze) {
+//
+//
+//    }
 
-        for (int i = 0; i < myarray.length; i++) {
-                 adjacencyList.add(new ArrayList<>());
-        }
-        for (int i = 0; i < myarray[0].length; i++) {
-            for (int j = 0; j < myarray.length; j++) {
-                if (myarray[i][j] == '|') {
-                    adjacencyList.get(i).add(j);
-                }
-            }
-        }
-            return adjacencyList;
-    }
 
     public static int[] findS(char[][] maze){
         int[] s = {0,0};
